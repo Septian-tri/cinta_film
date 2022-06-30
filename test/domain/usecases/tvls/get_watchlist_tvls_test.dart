fdce9fcce08566ff4890260867bb20e46f3027c1
Septian-tri <septiantriwidian@gmail.com>
2022-06-30 21:18:01 +0700

    fixe bug coverage

--- test/domain/usecases/tvls/get_watchlist_tvls_test.dart
+++ test/domain/usecases/tvls/get_watchlist_tvls_test.dart
@@ -1,25 +1,27 @@
-import 'package:cinta_film/domain/usecases/tvls/get_watchlist_status_tvls.dart';
+import 'package:dartz/dartz.dart';
+import 'package:cinta_film/domain/usecases/tvls/get_watchlist_tvls.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
+import '../../../dummy_data/dummy_objects_tvls.dart';
 import '../../../helpers/test_helper_tvls.mocks.dart';
 
 void main() {
-  late ClassStatusDaftarTontonTvls usecase;
+  late GetwatchlistTvls usecase;
   late MockTvlsRepository mockTvRepository;
 
   setUp(() {
     mockTvRepository = MockTvlsRepository();
-    usecase = ClassStatusDaftarTontonTvls(mockTvRepository);
+    usecase = GetwatchlistTvls(mockTvRepository);
   });
 
-  test('should get watchlist tv status from repository', () async {
+  test('should get list of tv series from the repository', () async {
     // arrange
-    when(mockTvRepository.isAddedTowatchlistTv(1))
-        .thenAnswer((_) async => true);
+    when(mockTvRepository.getwatchlistTv())
+        .thenAnswer((_) async => Right(testTvList));
     // act
-    final result = await usecase.execute(1);
+    final result = await usecase.execute();
     // assert
-    expect(result, true);
+    expect(result, Right(testTvList));
   });
 }
